17ce43034ba5d701db2c786989bc28eb2676a448
KendallCR01 <kendall.tencio.paniagua@est.una.ac.cr>
2021-03-17 20:28:03 -0600

    probando3

---
+++ persona.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tieso on 3/17/2021.
+//
+
+#include "persona.h"
---
+++ persona.h
@@ -0,0 +1,15 @@
+//
+// Created by Tieso on 3/17/2021.
+//
+
+#ifndef PROBANDO_PERSONA_H
+#define PROBANDO_PERSONA_H
+
+
+class persona {
+
+    //olaaaaaaaaaaaaaaaaaaa
+};
+
+
+#endif //PROBANDO_PERSONA_H
